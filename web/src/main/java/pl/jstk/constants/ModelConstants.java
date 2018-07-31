f51509ad962b01b0e7b248c9e65d140602585d43
epatsli <PATRSLIW@CE24519.corp.capgemini.com>
2018-07-31 11:01:46 +0200

    Add book

--- web/src/main/java/pl/jstk/constants/ModelConstants.java
+++ web/src/main/java/pl/jstk/constants/ModelConstants.java
@@ -9,6 +9,6 @@ public final class ModelConstants implements Serializable {
 	public static final String MESSAGE = "message";
 	public static final String INFO = "info";
 	public static final String bookList = "bookList";
-	public static final String bookid = "book.id";
-	// public static final String newBook = "newBook";
+	public static final String bookdelete = "bookDelete";
+	public static final String newBook = "newBook";
 }
---
+++ web/src/main/java/pl/jstk/controller/BookController.java
@@ -0,0 +1,59 @@
+package pl.jstk.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import pl.jstk.constants.ModelConstants;
+import pl.jstk.service.impl.BookServiceImpl;
+import pl.jstk.to.BookTo;
+
+@Controller
+public class BookController {
+
+	@Autowired
+	private BookServiceImpl bookService;
+
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public String showListBooks(Model model) {
+
+		List<BookTo> bookList = bookService.findAllBooks();
+		model.addAttribute(ModelConstants.bookList, bookList);
+		return "books";
+	}
+
+	@RequestMapping(value = "books/{bookId}", method = RequestMethod.GET)
+	public String showDetailsBook(@RequestParam("id") Long id, Model model) {
+		BookTo bookById = bookService.findBookById(id);
+		model.addAttribute("book", bookById);
+		return "book";
+	}
+
+	@DeleteMapping(value = "books/bookDelete/{bookId}")
+	public String deleteBook(@RequestParam("id") Long id, Model model) {
+		bookService.deleteBook(id);
+		model.addAttribute(ModelConstants.bookdelete, "The book you selected was removed");
+		return showListBooks(model);
+	}
+
+	@GetMapping("/books/add")
+	public String addBook(Model model) {
+		model.addAttribute("newBook", new BookTo());
+		return "addBook";
+	}
+
+	@PostMapping("/greeting")
+	public String addBook(@ModelAttribute("newBook") BookTo book, Model model) {
+		bookService.saveBook(book);
+		return showListBooks(model);
+	}
+}
